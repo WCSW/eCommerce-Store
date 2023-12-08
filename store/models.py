d1eb502ed696ce1f8390bd3d5721c5568bb1dac7
Channa Weerakoon <weerakoonchanna@gmail.com>
2023-12-09 01:00:20 +0530

    make first migration

--- store/models.py
+++ store/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -10,3 +11,25 @@ class Category(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name="product", on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="product_creatoe")
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255, default="admin")
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to="images/")
+    slug = models.SlugField(max_length=255)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    in_stock = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "Products"
+        ordering = ("-created",)
+
+    def __str__(self) -> str:
+        return self.title
